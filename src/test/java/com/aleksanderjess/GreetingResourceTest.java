9637e91910f6897960a28e8d21000960e9ad3b26
jessaleks <jess.aleksander@gmail.com>
2023-11-28 23:26:07 +0100

    fixed the test

--- src/test/java/com/aleksanderjess/GreetingResourceTest.java
+++ src/test/java/com/aleksanderjess/GreetingResourceTest.java
@@ -12,10 +12,10 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+                .when().get("/api/image?imageUrl=https%3A%2F%2Fik.imagekit.io%2F1dljx5jdv9do%2Fblog%2Fsuplementy.jpg&w=1920&q=75")
+                .then()
+                .statusCode(200);
+
     }
 
 }
